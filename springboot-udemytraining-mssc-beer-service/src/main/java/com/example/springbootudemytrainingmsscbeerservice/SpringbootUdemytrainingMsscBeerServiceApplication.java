07326517141a22960ee2d62501847090a7969aca
siree <siree@Siri>
2023-01-03 13:40:53 +0530

    First commit
    Change-Id: Ia3e58f52ed65892a5ada5bba85e454624a11d16a
    Signed-off-by: siree <siree@Siri>

---
+++ springboot-udemytraining-mssc-beer-service/src/main/java/com/example/springbootudemytrainingmsscbeerservice/SpringbootUdemytrainingMsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootudemytrainingmsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUdemytrainingMsscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUdemytrainingMsscBeerServiceApplication.class, args);
+	}
+
+}
